67c73337007c6fdbb178c9d2c27a2b64679822b5
onlinesmit <onlinesmitg@gmail.com>
2023-10-26 00:38:53 +0530

    New file added size of digit

---
+++ newCountSize.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+ int n= 23456;
+
+ printf("%d", sizeof(n));
+    // int count =0;
+
+    // while(n>0){
+    //     int rem = n%10;
+    //     count = count +rem;
+    //     n = n/10;
+    // }
+    // printf("%d", count);
+    return 0;
+}
